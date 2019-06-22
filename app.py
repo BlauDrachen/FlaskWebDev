bb3c929fbc64322b6fb3929174625c0bcd0228a9
Nathaniel Riehl <nate@nateriehl.com>
2019-06-22 11:02:21 -0500

    Chapter 3a: implementing basic templates

--- app.py
+++ app.py
@@ -1,27 +1,13 @@
-from flask import Flask
-from flask import request
-from flask import make_response
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>Hello World!</h1><p>Your browser is {}</p>'.format(user_agent)
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
-
-@app.route('/cookie')
-def cookie():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', 42)
-    return response
-
-@app.route('/req')
-def req():
-    return '<h1>Bad Request</h1>', 400
-
+    return render_template('user.html', name=name)
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
