4de0d170cc442d4545182500c1a6860ec81090c9
Nathaniel Riehl <nate@nateriehl.com>
2019-06-23 16:42:58 -0500

    Chapter 5a: Add database models and ORM to app.py

--- app.py
+++ app.py
@@ -7,15 +7,47 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+# Database imports
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+
+# globals
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'W8KDrAC$Rp54qLkC2zR0ACss'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+
+
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
-# globals
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'W8KDrAC$Rp54qLkC2zR0ACss'
-bootstrap = Bootstrap(app)
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 # routes
 @app.route('/', methods=['GET', 'POST'])
