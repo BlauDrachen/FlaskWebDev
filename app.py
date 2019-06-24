d231c049a981475fcabf78db704ccc545dfb8429
Nathaniel Riehl <nate@nateriehl.com>
2019-06-23 22:51:02 -0500

    Chapter 6b: Fix small concatenation issue

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
-app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <' + app.config['FLASKY_ADMIN'] + '>'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@gmail.com>'
 
 
 # other config
