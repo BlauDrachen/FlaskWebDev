fd42d0a8b1a645c0b1e88be57e7e9a513fbc2f25
Nathaniel Riehl <nate@nateriehl.com>
2019-06-23 22:46:15 -0500

    Chapter 6: Implement email and multi-threading

--- app.py
+++ app.py
@@ -1,28 +1,59 @@
 # Imports
+import os
+from threading import Thread
+# flask
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
-
-# WTForms imports
+from flask_mail import Mail, Message
+# WTForms
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
-# Database imports
-import os
+# Database
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-# globals
+# shared variables
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# app config
 app = Flask(__name__)
+# app config - wtforms
 app.config['SECRET_KEY'] = 'W8KDrAC$Rp54qLkC2zR0ACss'
+# app config - sqlalchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app config - mail
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <' + app.config['FLASKY_ADMIN'] + '>'
 
+
+# other config
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
+mail = Mail(app)
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'],
+                  recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 
 class NameForm(FlaskForm):
@@ -61,6 +92,8 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
@@ -79,5 +112,6 @@ def internal_server_error(e):
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
+
 # if __name__ == '__main__':
 #     app.run(debug=True)
