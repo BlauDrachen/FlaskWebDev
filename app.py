8c57a70f77e34602882af96d38819d31ef88fbb4
Nathaniel Riehl <nate@nateriehl.com>
2019-06-23 21:37:27 -0500

    Chapter 5d: Add Database migration support

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from wtforms.validators import DataRequired
 # Database imports
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # globals
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -20,6 +21,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
 
 
