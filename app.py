f81a2054e174219940cde5add3ad1c068f57e95f
Nathaniel Riehl <nate@nateriehl.com>
2019-06-23 20:54:21 -0500

    Chapter 5b: Add data access from sqlite

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from wtforms.validators import DataRequired
 import os
 from flask_sqlalchemy import SQLAlchemy
 
-
 # globals
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -54,12 +53,18 @@ class User(db.Model):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 @app.errorhandler(404)
 def page_not_found(e):
