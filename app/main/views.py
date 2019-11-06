ac58592b8e8167a3a6ae6155768589d69f916bcf
Nathaniel Riehl <nate@nateriehl.com>
2019-11-05 22:51:13 -0600

    Chapter 12b: follow posts with a join

--- app/main/views.py
+++ app/main/views.py
@@ -115,6 +115,7 @@ def follow(username):
         flash('You are already following this user.')
         return redirect(url_for('.user', username=username))
     current_user.follow(user)
+    db.session.commit()
     flash('You are now following %s.' % username)
     return redirect(url_for('.user', username=username))
 
@@ -131,6 +132,7 @@ def unfollow(username):
         flash('You are not following this user.')
         return redirect(url_for('.user', username=username))
     current_user.unfollow(user)
+    db.session.commit()
     flash('You are not following %s anymore.' % username)
     return redirect(url_for('.user', username=username))
 
@@ -152,7 +154,7 @@ def followers(username):
                            follows=follows)
 
 
-@main.route('/followed-by/<username>')
+@main.route('/followed_by/<username>')
 def followed_by(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
